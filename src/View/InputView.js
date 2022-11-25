e96152fc8a86dd1d20b663addd77b8ec661b9fed
SWARVY <swarvy0826@naver.com>
2022-11-26 02:12:34 +0900

    feat : 입,출력 뷰 기능구현

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,7 +1,21 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const InputView = {
-  readPlayerCommand() {},
+  readPlayerCommand(callback) {
+    Console.readLine('숫자를 입력해주세요 : ', (input) => {
+      callback(input);
+    });
+  },
 
-  readRetryCommand() {},
+  readRetryCommand(callback) {
+    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (input) => {
+      callback(input);
+    });
+  },
+
+  exit() {
+    return Console.close();
+  },
 };
+
+module.exports = InputView;
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,7 +1,17 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const OutputView = {
-  printStatus() {},
+  printStart() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  },
 
-  printResult() {},
+  printStatus(ballCounts) {
+    Console.print(ballCounts);
+  },
+
+  printClear() {
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+  },
 };
+
+module.exports = OutputView;
