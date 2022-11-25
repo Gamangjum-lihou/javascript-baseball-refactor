4b9fa3bb8d5ddfccee65a3d446973bb5325a2756
SWARVY <swarvy0826@naver.com>
2022-11-26 02:11:01 +0900

    feat : Player model 생성

--- src/Model/Player.js
+++ src/Model/Player.js
@@ -1,3 +1,24 @@
-class Player {}
+class Player {
+  #numbers;
+
+  #ballCounts;
+
+  storeNumber(numbers) {
+    this.#numbers = numbers;
+    console.log(this.#numbers);
+  }
+
+  storeBallCounts(ballCounts) {
+    this.#ballCounts = ballCounts;
+  }
+
+  getNumber() {
+    return this.#numbers;
+  }
+
+  getBallCounts() {
+    return this.#ballCounts;
+  }
+}
 
 module.exports = Player;
