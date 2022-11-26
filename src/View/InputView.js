0e47ce13aa4a1e307863b9792ced077abbf3b91c
SWARVY <swarvy0826@naver.com>
2022-11-26 19:26:55 +0900

    feat : 스트라이크,볼,낫싱 출력 구현 및 상수분리

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,14 +1,15 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { GAME_MESSAGE } = require('../Constants/Message');
 
 const InputView = {
   readPlayerCommand(callback) {
-    Console.readLine('숫자를 입력해주세요 : ', (input) => {
+    Console.readLine(GAME_MESSAGE.inGame, (input) => {
       callback(input);
     });
   },
 
   readRetryCommand(callback) {
-    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (input) => {
+    Console.readLine(GAME_MESSAGE.option, (input) => {
       callback(input);
     });
   },
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,16 +1,20 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { GAME_MESSAGE } = require('../Constants/Message');
 
 const OutputView = {
   printStart() {
-    Console.print('숫자 야구 게임을 시작합니다.');
+    Console.print(GAME_MESSAGE.start);
   },
 
   printStatus(ballCounts) {
-    Console.print(ballCounts);
+    if (ballCounts.strike && ballCounts.ball) Console.print(`${ballCounts.ball}볼 ${ballCounts.strike}스트라이크`);
+    if (ballCounts.strike && !ballCounts.ball) Console.print(`${ballCounts.strike}스트라이크`);
+    if (!ballCounts.strike && ballCounts.ball) Console.print(`${ballCounts.ball}볼`);
+    if (!ballCounts.strike && !ballCounts.ball) Console.print('낫싱');
   },
 
   printClear() {
-    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    Console.print(GAME_MESSAGE.end);
   },
 };
 
