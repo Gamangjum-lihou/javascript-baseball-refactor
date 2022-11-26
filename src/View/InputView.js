7b5069978c584a8c00b869168eb4a9cb7be9032c
SWARVY <swarvy0826@naver.com>
2022-11-26 19:50:46 +0900

    feat : 유효성 검사 기능 구현

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,15 +1,18 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { GAME_MESSAGE } = require('../Constants/Message');
+const { checkCorrectNumber, checkCorrectCommand } = require('../Util/Validation');
 
 const InputView = {
   readPlayerCommand(callback) {
     Console.readLine(GAME_MESSAGE.inGame, (input) => {
+      checkCorrectNumber(input);
       callback(input);
     });
   },
 
   readRetryCommand(callback) {
     Console.readLine(GAME_MESSAGE.option, (input) => {
+      checkCorrectCommand(input);
       callback(input);
     });
   },
