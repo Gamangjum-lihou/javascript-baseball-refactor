80877fe8162d6c87dfbb993aee0018af456e11a0
minjaeKim <smallkdb@gmail.com>
2022-11-29 22:55:01 +0900

    feat(BaseBallNumberMaker): 랜덤한 숫자 야구 정답을 만들어 내는 객체 구현

---
+++ src/BaseBallNumberMaker.js
@@ -0,0 +1,13 @@
+const { Random } = require('@woowacourse/mission-utils');
+
+const BaseBallNumberMaker = {
+	makeRandomNumbers(length) {
+		const RandomNumberSet = new Set();
+		while (RandomNumberSet.size < length) {
+			RandomNumberSet.add(Random.pickNumberInRange(1, 9));
+		}
+		return [...RandomNumberSet].join('');
+	},
+};
+
+module.exports = BaseBallNumberMaker;
