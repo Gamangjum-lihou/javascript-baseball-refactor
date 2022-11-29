58f99d0e20fdd03b1943a8f5e0658db536b41195
minjaeKim <smallkdb@gmail.com>
2022-11-30 00:30:57 +0900

    refactor(IoView): 프로그램을 종료하는 printEnd 메서드 추가

--- src/view/IoView.js
+++ src/view/IoView.js
@@ -1,21 +1,24 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const IoView = {
-	readNumber(callback) {
-		Console.readLine('숫자를 입력해주세요 : ', callback);
-	},
-	readRetry(callback) {
-		Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', callback);
-	},
-	printStart() {
-		Console.print('숫자 야구 게임을 시작합니다.');
-	},
-	printCount(ball, strike) {
-		Console.print(ball, strike);
-	},
-	printFinish() {
-		Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-	},
+  readNumber(callback) {
+    Console.readLine('숫자를 입력해주세요 : ', callback);
+  },
+  readRetry(callback) {
+    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', callback);
+  },
+  printStart() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  },
+  printCount(ballCount) {
+    Console.print(ballCount);
+  },
+  printFinish() {
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+  },
+  printEnd() {
+    Console.close();
+  },
 };
 
 module.exports = IoView;
