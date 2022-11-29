72c45fb0fa2745c859e5e41e772359ea9c69df58
minjaeKim <smallkdb@gmail.com>
2022-11-30 00:53:05 +0900

    style(BaseBallNumberMaker): 코드 스타일 통일

--- __tests__/BaseBallNumberMakerTest.js
+++ __tests__/BaseBallNumberMakerTest.js
@@ -1,23 +1,23 @@
 const { Random } = require('@woowacourse/mission-utils');
-const { makeRandomNumbers } = require('../src/BaseBallNumberMaker');
+const { makeRandomNumbers } = require('../src/model/utils/BaseBallNumberMaker');
 
 const mockRandoms = (numbers) => {
-	Random.pickNumberInRange = jest.fn();
-	numbers.reduce((acc, number) => acc.mockReturnValueOnce(number), Random.pickNumberInRange);
+  Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => acc.mockReturnValueOnce(number), Random.pickNumberInRange);
 };
 
 test('makeRandomNumbers가 제대로 작동하는지 확인. case1', () => {
-	mockRandoms([1, 3, 5]);
+  mockRandoms([1, 3, 5]);
 
-	const result = makeRandomNumbers(3);
+  const result = makeRandomNumbers(3);
 
-	expect(result).toEqual('135');
+  expect(result).toEqual('135');
 });
 
 test('makeRandomNumbers가 제대로 작동하는지 확인. case2', () => {
-	mockRandoms([1, 3, 5, 5]);
+  mockRandoms([1, 3, 5, 5]);
 
-	const result = makeRandomNumbers(3);
+  const result = makeRandomNumbers(3);
 
-	expect(result).toEqual('135');
+  expect(result).toEqual('135');
 });
