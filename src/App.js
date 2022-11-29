cf8dd34a41c84559966600933e1d729371bebaa9
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-29 21:42:40 +0900

    :sparkles: feat : App에서 controller 정의

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const Controller = require('./Controller');
+const View = require('./View');
+const Model = require('./Model');
+
 class App {
-  play() {}
+  play() {
+    const view = new View();
+    const model = new Model();
+    this.controller = new Controller(model, view);
+    this.controller.start();
+  }
 }
 
 module.exports = App;
