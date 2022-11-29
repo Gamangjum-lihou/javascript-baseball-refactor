ad3ba1e46c8062eda2d3638ba98238ca7950f959
y00eunji <aine0227@naver.com>
2022-11-30 01:24:33 +0900

    feat: 이긴 경우 종료 명령어 받기

--- src/view/View.js
+++ src/view/View.js
@@ -1,5 +1,7 @@
+const { Console } = require('@woowacourse/mission-utils');
 const { input } = require('./InputView');
 const { print } = require('./OutputView');
+
 const Messages = require('../utils/Messages');
 
 const View = {
@@ -15,8 +17,16 @@ const View = {
     print(message);
   },
 
+  win() {
+    print(Messages.RESULT_SUCCESS);
+  },
+
   inputCommand(callback) {
     input(Messages.INPUT_COMMAND, callback);
+  },
+
+  end() {
+    Console.close();
   },
 };
 
