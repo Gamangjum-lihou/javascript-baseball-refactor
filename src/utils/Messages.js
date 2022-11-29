e7228d87630e0a6dda14c1d3049fc3b0d0b805df
y00eunji <aine0227@naver.com>
2022-11-30 01:06:27 +0900

    fix: 게임 메시지 상수 수정

--- src/utils/Messages.js
+++ src/utils/Messages.js
@@ -1,13 +1,11 @@
 const Messages = Object.freeze({
   GAME_START: '숫자 야구 게임을 시작합니다.',
   INPUT_NUMBERS: '숫자를 입력하세요 : ',
-  GONG_INVALID_FORMAT: '서로 다른 3개의 숫자를 입력해야합니다.',
-  GUESS_GONG_RESULT_BALL: '볼',
-  GUESS_GONG_RESULT_STRIKE: '스트라이크',
-  GUESS_GONG_RESULT_NOTHING: '낫싱',
-  GUESS_GONG_RESULT_SUCCESS: '3개의 숫자를 모두 맞혔습니다! 게임 종료',
-  END_SELECT: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
-  END_SELECT_INVALID_FORMAT: '1 또는 2를 입력하세요.',
+  RESULT_BALL: num => `${num}볼`,
+  RESULT_STRIKE: num => `${num}스트라이크`,
+  RESULT_NOTHING: '낫싱',
+  RESULT_SUCCESS: '3개의 숫자를 모두 맞혔습니다! 게임 종료',
+  INPUT_COMMAND: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
 });
 
 module.exports = Messages;
