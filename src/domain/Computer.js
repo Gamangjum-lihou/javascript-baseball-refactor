bab342c31842c9e7d7846e890a6330ca39e3a536
y00eunji <aine0227@naver.com>
2022-11-30 01:07:57 +0900

    feat: 랜덤 숫자 생성, 사용자 입력값과 비교 구현

--- src/domain/Computer.js
+++ src/domain/Computer.js
@@ -1,10 +1,33 @@
-const { getNumber } = require('../utils/Random');
+const { getNumber } = require('../utils/generateNumber');
 
 class Computer {
   #numbers;
 
   generateNumber() {
     this.#numbers = getNumber();
+    console.log(this.#numbers);
+  }
+
+  getScore(input) {
+    const computer = [...this.#numbers];
+    const score = { ball: 0, strike: 0 };
+
+    [...input].map(Number).forEach((num, index) => {
+      if (this.#isStrike(num, computer, index)) score.strike += 1;
+      if (this.#isBall(num, computer, index)) score.ball += 1;
+    });
+
+    console.log(score.strike);
+
+    return score;
+  }
+
+  #isStrike(num, computer, index) {
+    return num === computer[index];
+  }
+
+  #isBall(num, computer, index) {
+    return !this.#isStrike(num, computer, index) && this.#numbers.has(num);
   }
 }
 
